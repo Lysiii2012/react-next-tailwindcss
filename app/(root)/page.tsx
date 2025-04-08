ecaaf9a3ee621112ea6ae5ef2db772d33da8ae1d
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2025-04-08 16:26:15 +0300

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { title } from "process";
+
+export const metadata = {
+  title: "Головна"
+}
+
+const HomePage = () => {
+  return ( 
+    <Button>Home</Button>
+   )
+}
+
+export default HomePage;
---
+++ lib/contants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Store'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Iнтернет магазин"
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
